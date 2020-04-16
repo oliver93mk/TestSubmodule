0bf2ab05fd64acc24ddfe0eb52175754579b2a82
Mike Kentros <mike.kentros@gmail.com>
2020-04-17 01:27:18 +0300

    Added a class for referencing

---
+++ MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass {
+
+	public bool mpla;
+
+    MyClass()
+    {
+        mpla=false;
+    }
+}
